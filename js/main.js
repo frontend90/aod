2dcacf006df09e91568b463a4d68826bb9153cd2
Vasiliy Pupkin <vpupkin@company.com>
2020-09-13 16:28:52 +0300

    верстка

--- js/main.js
+++ js/main.js
@@ -13,3 +13,13 @@ document.documentElement.addEventListener("click", function (e) {
     user_icon.classList.remove('active');
   }
 });
+
+/*************    ibg    ********************/
+function ibg() {
+  $.each($('.ibg'), function (index, val) {
+    if ($(this).find('img').length > 0) {
+      $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+    }
+  });
+}
+ibg();
